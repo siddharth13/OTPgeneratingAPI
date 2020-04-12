60b0dde244d4216dc206705af29aef6592430845
siddharth gupta <gsiddharth79@gmail.com>
2020-04-12 13:41:38 +0530

    first

--- src/main/java/application_package/Controller.java
+++ src/main/java/application_package/Controller.java
@@ -39,16 +39,17 @@ public class Controller {
     public String Home(){
         return "endpoints------generatingOTP-->type=post(for inserting value";
     }
-    @PostMapping(path = "/generatingOTP")
+    @PostMapping(path = "/generateOTP")
     public String generate(@RequestBody RequestData requestData){
             Table table = dynamoDB.getTable("OTPDatabase");
             RequestData rd=requestData;
             String s=rd.getId();
             Random rand = new Random();
             String otp= ""+(rand.nextInt(899999) + 100000);
+            String timeinmilli=String.valueOf(System.currentTimeMillis());
             try {
                 PutItemOutcome outcome = table
-                        .putItem(new Item().withPrimaryKey("ID",s).with("OTP",otp));
+                        .putItem(new Item().withPrimaryKey("ID",s).with("OTP",otp).with("TIMESTAMP",timeinmilli));
             }
             catch (Exception e){
                 System.err.println("Unable to add item: " + s + " " + otp);
@@ -56,28 +57,58 @@ public class Controller {
             }
      return otp;
     }
-    @PostMapping("/verify")
+    @PostMapping("/verifyOTP")
     public String verify(@RequestBody Data input) {
         Table table = dynamoDB.getTable("OTPDatabase");
         GetItemSpec spec = new GetItemSpec().withPrimaryKey("ID", input.getId());
 
             try {
                 Item outcome = table.getItem(spec);
-                if(outcome==null){
+                if (outcome == null) {
                     return "User not found";
+                } else {
+
+                long currenttime=System.currentTimeMillis();
+                long timestamp= Long.parseLong(outcome.get("TIMESTAMP").toString());
+                if(currenttime-timestamp>180000){
+                    return "OTP expired";
                 }
+
                 if (outcome.get("OTP").toString().equals(input.getGeneratedOTP())) {
                     return "OTP verified";
                 } else {
                     return "Wrong OTP";
                 }
+
+
+
+
+            }
             } catch (Exception e) {
                 System.err.println(e.getMessage());
                 return "Connection Error";
             }
+    }
+    @PostMapping("/regenerateOTP")
+    public String regenerate(@RequestBody RequestData requestData){
+        Table table = dynamoDB.getTable("OTPDatabase");
+        GetItemSpec spec = new GetItemSpec().withPrimaryKey("ID", requestData.getId());
 
+        try {
+            Item outcome = table.getItem(spec);
+            if(outcome==null){
+                return "User not found";
+            }
+           else{
 
-
+                String time=String.valueOf(System.currentTimeMillis());
+                table.putItem(new Item().withPrimaryKey("ID",requestData.getId()).with("OTP",outcome.get("OTP").toString()).with("TIMESTAMP",time));
+                return outcome.get("OTP").toString();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return "Connection Error";
+        }
     }
 
 }
